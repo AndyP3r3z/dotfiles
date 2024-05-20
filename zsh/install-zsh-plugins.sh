2536f3513d1a378cf61b2f60890e705507d7bbca
Andy Pérez <andy.perez.amado@gmail.com>
2024-05-20 17:22:55 -0600

    INITIAL COMMIT.

---
+++ zsh/install-zsh-plugins.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+# 
+# Date:	2024/05/20
+# Author:  andyp3r3z
+#
+# Installs zsh-autopair, zsh-autosuggestions and zsh-syntax-highlighting.
+
+git clone https://github.com/hlissner/zsh-autopair.git
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
