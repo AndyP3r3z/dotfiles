e97b9ff028a3647aa1d2c4ce271fb00dd68dd4d9
AndyP3r3z <andy.perez.amado@gmail.com>
2024-11-03 17:24:06 -0600

    ADDED: Neovim (first) configs.

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+-- require("everforest")
+require("mappings")
+
+local colorscheme = "everforest"
+local options = {
+    -- ...others configs...
+    termguicolors = true -- Enables 24-bit RGB color
+}
+vim.o.background = "dark"
+vim.g.everforest_background = "hard"
---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,5 @@
+-- Actually not insane defaults
+-- It is missing: use h in normal mode to change to insert mode.
+vim.keymap.set('n', 'i', '<Up>')
+vim.keymap.set('n', 'k', '<Down>')
+vim.keymap.set('n', 'j', '<Left>')
