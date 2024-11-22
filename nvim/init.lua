d68803bf1f82cfb8ffa49bbe2091e35c8d4c7971
Andy Pérez <andy.perez.amado@gmail.com>
2024-11-22 09:37:12 -0600

    CHANGED: nvim StatusLine colors.

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,5 @@ local options = {
     termguicolors = true -- Enables 24-bit RGB color
 }
 
+vim.api.nvim_set_hl(0, "StatusLine", { bg="#a7c080", fg="#1e2326" })
+vim.api.nvim_set_hl(0, "StatusLineNC", {bg="#374145", fg="#859289" })
