4c81aac8582c2c1ee52d9cf8c9986cc4e34d9dde
Andy Pérez <andy.perez.amado@gmail.com>
2025-01-18 13:50:24 -0600

    CHANGED: 'U' to nvim redo.

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -10,4 +10,4 @@ vim.opt.whichwrap:append {
 
 -- Map <C-c> to copy selected text to the clipboard in Visual Mode
 vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true })
-
+vim.keymap.set('n', 'U', '<C-r>', { noremap = true })
