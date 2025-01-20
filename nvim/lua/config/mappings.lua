43047395788c7175dea2d3d7064ff2f6d75367d3
Andy Pérez <andy.perez.amado@gmail.com>
2025-01-19 19:33:59 -0600

    ADDED: mapleader as space.

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,4 +1,5 @@
 -- Wrap arrows to previous and next lines
+vim.g.mapleader = " "
 vim.opt.whichwrap:append {
 	['h'] = true,
 	['l'] = true,
@@ -10,4 +11,5 @@ vim.opt.whichwrap:append {
 
 -- Map <C-c> to copy selected text to the clipboard in Visual Mode
 vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true })
+-- Undo with U
 vim.keymap.set('n', 'U', '<C-r>', { noremap = true })
