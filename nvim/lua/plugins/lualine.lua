86e800dc9f327d71c82e28035afb71ee32c69d00
Andy Pérez <andy.perez.amado@gmail.com>
2025-07-04 14:07:01 -0600

    ADDED: Lualine word count.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,5 +2,20 @@ return {{
 	enabled=true,
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	opts = {}
+	opts = {
+		sections = {
+			lualine_c = {
+				'filename',
+				{
+					function()
+						return tostring(vim.fn.wordcount().words) .. " words"
+					end,
+					cond = function()
+						local ft = vim.bo.filetype
+						return ft == "markdown" or ft == "typst" or ft == "latex" or ft == "asciidoc"
+					end,
+				},
+			}
+		}
+	}
 }}
