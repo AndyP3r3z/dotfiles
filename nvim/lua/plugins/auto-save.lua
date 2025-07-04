85a5f82db7392bacc743be18dbf74f535d3ede71
Andy Pérez <andy.perez.amado@gmail.com>
2025-07-04 14:31:01 -0600

    ADDED: Autosave for Document files.

---
+++ nvim/lua/plugins/auto-save.lua
@@ -0,0 +1,12 @@
+return {{
+	"okuuva/auto-save.nvim",
+	version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+	cmd = "ASToggle", -- optional for lazy loading on command
+	event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+	opts = {
+		condition = function(buf)
+			local ft = vim.bo.filetype
+			return ft == "markdown" or ft == "typst" or ft == "latex" or ft == "asciidoc"
+		end,
+	},
+}}
