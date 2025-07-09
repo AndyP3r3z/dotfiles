39a8e2ffb8e671afbeaff1d7e7e47f0c2977cc19
Andy Pérez <andy.perez.amado@gmail.com>
2025-07-09 14:21:01 -0600

    ADDED: Window navigation in Neovim.

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -1,3 +1,17 @@
+-- Commands to tell kitty Neovim's active.
+vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
+	group = vim.api.nvim_create_augroup("KittySetVarVimEnter", { clear = true }),
+	callback = function()
+		io.stdout:write("\x1b]1337;SetUserVar=in_editor=MQo\007")
+	end,
+})
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+	group = vim.api.nvim_create_augroup("KittyUnsetVarVimLeave", { clear = true }),
+	callback = function()
+		io.stdout:write("\x1b]1337;SetUserVar=in_editor\007")
+	end,
+})
+
 -- Prompt to save when trying to quit with unsaved changes
 vim.api.nvim_create_user_command("Q", function(opts)
 	-- Check `!` option
--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,4 +1,8 @@
 -- Wrap arrows to previous and next lines
+
+local keymap = vim.keymap
+local options = { noremap = true, silent = true }
+
 vim.g.mapleader = " "
 vim.opt.whichwrap:append {
 	['h'] = true,
@@ -9,14 +13,18 @@ vim.opt.whichwrap:append {
 	[']'] = true,
 }
 
--- Map <C-c> to copy selected text to the clipboard in Visual Mode
--- vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true })
 -- Undo with U
-vim.keymap.set('n', 'U', '<C-r>', { noremap = true })
+keymap.set('n', 'U', '<C-r>', { noremap = true })
 
 -- Use `<Tab>` and `<S-Tab>` for navigation through completion list
 local imap_expr = function(lhs, rhs)
-	vim.keymap.set('i', lhs, rhs, { expr = true })
+	keymap.set('i', lhs, rhs, { expr = true })
 end
 imap_expr('<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
 imap_expr('<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+
+-- Change window focus.
+keymap.set('n', '<C-h>', '<C-w>h', options)
+keymap.set('n', '<C-j>', '<C-w>j', options)
+keymap.set('n', '<C-k>', '<C-w>k', options)
+keymap.set('n', '<C-l>', '<C-w>l', options)
