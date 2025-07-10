f8cb940b932c9df69074dbd8d9648900eb886f8a
Andy Pérez <andy.perez.amado@gmail.com>
2025-07-10 08:47:18 -0600

    CHANGED: Angled to rounded Lualine.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,20 +2,26 @@ return {{
 	enabled=true,
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	opts = {
-		sections = {
-			lualine_c = {
-				'filename',
-				{
-					function()
-						return tostring(vim.fn.wordcount().words) .. " words"
-					end,
-					cond = function()
-						local ft = vim.bo.filetype
-						return ft == "markdown" or ft == "typst" or ft == "latex" or ft == "asciidoc"
-					end,
-				},
+	config = function()
+		require('lualine').setup({
+		options = {
+			component_separators = {left = "", right = ""},
+			section_separators = {left = "", right = ""},
+			sections = {
+				lualine_c = {
+					'filename',
+					{
+						function()
+							return tostring(vim.fn.wordcount().words) .. " words"
+						end,
+						cond = function()
+							local ft = vim.bo.filetype
+							return ft == "markdown" or ft == "typst" or ft == "latex" or ft == "asciidoc"
+						end,
+					},
+				}
 			}
 		}
-	}
+	})
+	end,
 }}
