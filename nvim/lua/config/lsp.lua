5197e6047cd8b7193544ee9019a8ce7d9cd98568
Andy Pérez <andy.perez.amado@gmail.com>
2025-07-12 10:29:30 -0600

    FIXED: no `gqq` possible due to GPLS taking control over formating.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -8,6 +8,19 @@ vim.lsp.enable({
 	"lua_ls",
 })
 
+-- Disable go formatting.
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(args)
+		-- Unset 'formatexpr'
+		-- vim.bo[args.buf].formatexpr = nil
+		vim.opt.formatprg = ''
+		-- Unset 'omnifunc'
+		-- vim.bo[args.buf].omnifunc = nil
+		-- Unmap K
+		-- vim.keymap.del('n', 'K', { buffer = args.buf })
+	end,
+})
+
 -- Diagnostics
 vim.diagnostic.config({
 	virtual_lines = { current_line = true },
